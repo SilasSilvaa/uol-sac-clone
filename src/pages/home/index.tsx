af9ec74ebfe05dafc721efa6777c2d86c24feeee
Silassilvaa <silas-oli@hotmail.com>
2025-04-06 21:22:15 -0300

    feat: create home page

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,9 +1,24 @@
+import { Card } from "./components/Card/Card";
+import { Carrousel } from "./components/Carrousel/Carrousel";
+import { DownloadApp } from "./components/DownloadApp/DownloadApp";
+import { Footer } from "./components/Footer/Footer";
+import { Header } from "./components/Header/Header";
+import { InfoComponent } from "./components/InfoComponent/InfoComponent";
+import { PreHeader } from "./components/PreHeader/PreHeader";
+import { Search } from "./components/Search/Search";
 
 export function Home() {
 
     return (
         <>
-            <h1>Hellp</h1>
+            <PreHeader />
+            <Header />
+            <Carrousel />
+            <InfoComponent />
+            <Card />
+            <Search />
+            <DownloadApp />
+            <Footer />
         </>
     )
 }
