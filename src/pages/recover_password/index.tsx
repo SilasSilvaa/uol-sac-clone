3f43e39be874d9bca4042cb06b2fe6b080f70453
silassilvaa <silas-oli@hotmail.com>
2025-05-13 09:47:32 -0300

    refactor: adding semantic to recover password page

--- src/pages/recover_password/index.tsx
+++ src/pages/recover_password/index.tsx
@@ -5,26 +5,40 @@ import { Link } from 'react-router-dom';
 export function RecoverPassword() {
     return (
         <main className='recover_container'>
-            <section className='recover_content'>
-                <div className='recover_route'>
-                    <img src="/house.svg" alt="" />
+            <section className='recover_content' aria-labelledby="recover-heading">
+                <header className='recover_route' aria-label="Breadcrumb">
+                    <img src="/house.svg" alt="Início" />
                     <Link to="/">Home</Link>
-                    <img src="/arrow_next.svg" alt="" />
-                    <span>Redefinição de senha</span>
-                </div>
-                <h1>Redefinição de senha</h1>
+                    <img src="/arrow_next.svg" alt="Próximo" />
+                    <span aria-current="page">Redefinição de senha</span>
+                </header>
 
-                <form action="" className='recover_form' >
-                    <span>Digite o <strong>e-mail, CPF</strong> ou <strong>CNPJ</strong> cadastrado no UOL.</span>
+                <h1 id="recover-heading">Redefinição de senha</h1>
+
+                <form className='recover_form'>
+                    <p>
+                        Digite o <strong>e-mail, CPF</strong> ou <strong>CNPJ</strong> cadastrado no UOL.
+                    </p>
+
                     <div className='recover_form_input'>
-                        <input type="text" name="" id="" placeholder='E-mail ou CPF/CNPJ' />
-                        <button>
-                            <img src="/question.svg" alt="" />
+                        <label htmlFor="userIdentifier" className="sr-only">E-mail, CPF ou CNPJ</label>
+                        <input
+                            type="text"
+                            id="userIdentifier"
+                            name="userIdentifier"
+                            placeholder="E-mail ou CPF/CNPJ"
+                            aria-required="true"
+                        />
+                        <button type="button" aria-label="Ajuda sobre o campo">
+                            <img src="/question.svg" alt="Ajuda" />
                         </button>
                     </div>
+
                     <div className='recover_form_buttons'>
-                        <button className='recover_form_button_continue'>Continuar</button>
-                        <button className='recover_form_button_goback'>
+                        <button type="submit" className='recover_form_button_continue'>
+                            Continuar
+                        </button>
+                        <button type="button" className='recover_form_button_goback'>
                             Voltar
                         </button>
                     </div>
