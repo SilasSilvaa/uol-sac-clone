f15583ca9198153efaf79a835194264c6a0b48d7
Silassilvaa <silas-oli@hotmail.com>
2025-05-12 22:15:01 -0300

    refactor: removing unused code

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,24 +1,17 @@
 import { Carrousel } from "./components/Carrousel/Carrousel";
 import { DownloadApp } from "./components/DownloadApp/DownloadApp";
-import { Footer } from "./components/Footer/Footer";
-import { Header } from "./components/Header/Header";
 import { InfoComponent } from "./components/InfoComponent/InfoComponent";
 import { CardsList } from "./components/ListCards/CardsList";
-import { PreHeader } from "./components/PreHeader/PreHeader";
 import { Search } from "./components/Search/Search";
 
 export function Home() {
-
     return (
         <>
-            <PreHeader />
-            <Header />
             <Carrousel />
             <InfoComponent />
             <CardsList />
             <Search />
             <DownloadApp />
-            <Footer />
         </>
     )
 }
