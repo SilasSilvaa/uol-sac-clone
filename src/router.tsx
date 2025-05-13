6a628550211ac79951a24fb6686ce79783a8abdc
Silassilvaa <silas-oli@hotmail.com>
2025-05-12 22:14:02 -0300

    feat: adding default layout and recover password page

--- src/router.tsx
+++ src/router.tsx
@@ -1,11 +1,16 @@
 import { Routes, Route } from "react-router-dom";
 import { Home } from "./pages/home";
 import { Login } from "./pages/login";
+import { RecoverPassword } from "./pages/recover_password";
+import { Default } from "./layout/Default";
 
 export function Router() {
     return (
         <Routes>
-            <Route path="/" element={< Home />} />
+            <Route path="/" element={<Default />}>
+                <Route path="/" element={<Home />} />
+                <Route path="/recoverpassword" element={<RecoverPassword />} />
+            </Route>
             <Route path="/login" element={<Login />} />
         </Routes>
     );
