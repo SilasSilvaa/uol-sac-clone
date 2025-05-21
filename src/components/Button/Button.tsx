858f572b96473a66a4ec8f130194881aca74a45d
Silassilvaa <silas-oli@hotmail.com>
2025-05-20 23:09:03 -0300

    feat: creating global button component

---
+++ src/components/Button/Button.tsx
@@ -0,0 +1,9 @@
+import { ComponentProps } from "react";
+
+
+type ButtonProps = ComponentProps<'button'> & {
+};
+
+export function Button({ ...props }: ButtonProps) {
+    return <button {...props} />;
+}
