12f2cb6128349c54c0c078f6b29b4e97c114d44b
silassilvaa <silas-oli@hotmail.com>
2025-06-03 20:12:28 -0300

    feat: new page debt settlement

---
+++ src/pages/debt_settlement/index.tsx
@@ -0,0 +1,13 @@
+import './index.css'
+
+export function DebtSettlement() {
+    return (
+        <main>
+            <section className='debt_container'>
+                <h1>Comprovante de quitação de débitos</h1>
+                <p><a href="">Acesse seu comprovante</a> de quitação contendo informações sobre o pagamento de todas as suas faturas.</p>
+                <p>Se não for mais assinante ou não tiver acesso ao SAC online, <a href="">veja os telefones</a> das centrais de atendimento.</p>
+            </section>
+        </main >
+    )
+}
