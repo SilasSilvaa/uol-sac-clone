8ae7eaa82796489e37ca3d98068ad28a118ca31e
Silassilvaa <silas-oli@hotmail.com>
2025-06-09 23:09:26 -0300

    feat: "header" component for the online support page

---
+++ src/pages/online_suport/components/SupportHeader.tsx
@@ -0,0 +1,11 @@
+import { BackHomeButton } from "./BackHomeButton";
+
+export function SupportHeader() {
+    return (
+        <div className='support_header'>
+            <BackHomeButton />
+            <h1>Atendimento online</h1>
+            <p>Entre em contato conosco selecionando uma das opções abaixo.</p>
+        </div>
+    );
+}
